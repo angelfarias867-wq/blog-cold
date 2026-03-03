1d7b695cda8fe35884ed37e298269a37b5431700
angelfarias867-wq <angelfarias867@gmail.com>
2026-03-03 13:21:43 -0400

    actualizacion del js

--- script2.js
+++ script2.js
@@ -1,6 +1,5 @@
 const preguntas = document.querySelectorAll('h3');
 
-// Recorremos cada <h3> y le agregamos la clase "cyan"
-preguntas.forEach(pregunta => {
-    pregunta.setAttribute("class", "cyan");
+document.querySelectorAll('h3').forEach(pregunta => {
+    pregunta.classList.add('cyan');
 });
